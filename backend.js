a5e370e3bd7e43926a01f9e24ea761d40b5a99e1
anarchaworld <anarchaworld@gmail.com>
2023-02-05 08:54:12 +0000

    mongodb added and all functionalities ported

--- backend.js
+++ backend.js
@@ -2,36 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
+const userServices = require("./models/user-services");
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
+// const users = {
+//   users_list: [
+//     {
+//       _id: "xyz789",
+//       name: "Charlie",
+//       job: "Janitor",
+//     },
+//     {
+//       _id: "abc123",
+//       name: "Mac",
+//       job: "Bouncer",
+//     },
+//     {
+//       _id: "ppp222",
+//       name: "Mac",
+//       job: "Professor",
+//     },
+//     {
+//       _id: "yat999",
+//       name: "Dee",
+//       job: "Aspring actress",
+//     },
+//     {
+//       _id: "zap555",
+//       name: "Dennis",
+//       job: "Bartender",
+//     },
+//   ],
+// };
 
 app.use(express.json());
 app.use(cors());
@@ -40,27 +41,35 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined && job != undefined) {
-    res.send(
-      users["users_list"].filter(
-        (user) => user["name"] === name && user["job"] === job
-      )
-    );
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
+  // if (name != undefined && job != undefined) {
+  //   res.send(
+  //     users["users_list"].filter(
+  //       (user) => user["name"] === name && user["job"] === job
+  //     )
+  //   );
+  // } else if (name != undefined) {
+  //   let result = findUserByName(name);
+  //   result = { users_list: result };
+  //   res.send(result);
+  // } else {
+  //   res.send(users);
+  // }
+  try {
+    const result = await userServices.getUsers(name, job);
+    res.send({ users_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
   }
 });
 
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  let result = findUserById(id);
+app.get("/users/:_id", async (req, res) => {
+  const _id = req.params["id"];
+  // let result = findUserBy_id(id);
+  const result = await userServices.findUserBy_id(id);
   if (result === undefined || result.length == 0) {
     res.status(404).send("Resource not found.");
   } else {
@@ -69,27 +78,41 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-app.post("/users", (req, res) => {
-  try {
-    var userToAdd = req.body;
-    userToAdd.id = genId();
-    addUser(userToAdd);
-    res.status(201).send(userToAdd);
-  } catch {
-    resstatus(400).send("failure to add a user");
+app.post("/users", async (req, res) => {
+  // try {
+  //   var userToAdd = req.body;
+  //   userToAdd._id = genId();
+  //   addUser(userToAdd);
+  //   res.status(201).send(userToAdd);
+  // } catch {
+  //   resstatus(400).send("failure to add a user");
+  // }
+  const user = req.body;
+  user._id = genId();
+  const savedUser = await userServices.addUser(user);
+  if (savedUser) {
+    res.status(201).send(savedUser);
+  } else {
+    res.status(500).end();
   }
 });
 
-app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  const index = users["users_list"].findIndex((user) => user.id === id);
-  if (index == -1) {
-    res.status(404).send("record not found");
-  } else {
-    users["users_list"].splice(index, 1);
+app.delete("/users/:_id", (req, res) => {
+  if (userServices.deleteUser(req.params["_id"])) {
     res.status(204).end();
+  } else {
+    res.status(404).send("record not found.");
   }
+  // const _id = req.params["id"];
+  // const index = users["users_list"].findIndex((user) => user._id === id);
+  // if (index == -1) {
+  //   res.status(404).send("record not found");
+  // } else {
+  //   users["users_list"].splice(index, 1);
+  //   res.status(204).end();
+  // }
 });
+
 function genId() {
   var all =
     "wlypbzfoucrstgmneiaqjvdkxhWLYPBZFOUCRSTGMNEIAQJVDKXH0123456789-_.~";
@@ -100,15 +123,17 @@ function genId() {
   }
   return res;
 }
-function addUser(user) {
-  users["users_list"].push(user);
-}
-function findUserById(id) {
-  return users["users_list"].find((user) => user["id"] === id);
-}
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
+
+// function addUser(user) {
+//   users["users_list"].push(user);
+// }
+// function findUserBy_id(id) {
+//   return users["users_list"].find((user) => user["_id"] === id);
+// }
+// const findUserByName = (name) => {
+//   return users["users_list"].filter((user) => user["name"] === name);
+// };
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
